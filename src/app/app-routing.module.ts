842d22eb441d2df16f97dd337793197fa32c1cf9
JesusDLF <191702@utags.edu.mx>
2020-08-03 00:44:40 -0500

    Se agregaron las pantallas de order-tracking-operation y el de tracking-order-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from './guards/auth.guard';
 import { ClientListComponent } from './pages/client-module/client-list/client-list.component';
 import { TrackingOrderListComponent } from './pages/order-tracking-module/tracking-order-list/tracking-order-list.component';
 import { DeviceDeliveryListComponent } from './pages/device-module/device-delivery-list/device-delivery-list.component';
+import { OrderTrackingOperationComponent } from './pages/order-tracking-module/order-tracking-operation/order-tracking-operation.component';
 
 
 
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   { path: 'client-list', component: ClientListComponent},
   { path: 'tracking-order-list', component: TrackingOrderListComponent},
+  { path: 'order-tracking-operation', component: OrderTrackingOperationComponent},
   { path: 'device-delivery-list', component: DeviceDeliveryListComponent},
   { path: '', pathMatch: 'full', redirectTo: 'principal' }
   
